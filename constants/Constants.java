9f4bf5c4d4e8f11f618b99c59c3eab9c9668a85d
schmithvillers <schmithvillers@Bhumikas-MacBook-Air.local>
2026-07-25 22:37:05 +0530

    adding constants

--- constants/Constants.java
+++ constants/Constants.java
@@ -1,1 +1,12 @@
-public static final String BILL_FILE = "data/bills.csv";
+package com.airtribe.meditrack.constants;
+
+public class Constants {
+
+    public static final double GST = 0.18;
+
+    public static final String DOCTOR_FILE = "data/doctors.csv";
+    public static final String PATIENT_FILE = "data/patients.csv";
+    public static final String APPOINTMENT_FILE = "data/appointments.csv";
+    public static final String BILL_FILE = "data/bills.csv";
+
+}
